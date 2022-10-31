7af475fc4b9e517ea0d16a3d8db52f19767288a7
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-10-31 16:29:26 +0100

    vue dealings

---
+++ src/components/Log_in.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <h1>{{msg}}</h1>
+</div> 
+<h1>Login</h1>
+ <div>
+    <input type="text" v-model="form.username" placeholder="Username"/>
+    <input type="password" v-model="form.password" placeholder="Password"/>
+    <button v-on:click = "login()">Login</button>
+ </div>
+</template>
+
+
+
+
+<script>
+import axios from 'axios'
+export default {
+  // name:'Log_in',
+  // props:{
+  //       msg: String
+  //     }
+  data() {
+    return {
+      form:{
+        username: '',
+        password:'',
+      }
+    }
+  },
+  methods: {
+    login() {
+       axios.post("https://usermanagementsyt.herokuapp.com/api/login-user",this.form)
+       .then(function(response){
+            if(response == 201){
+                this.$router.push({name:'Home'})
+            }
+       }.bind(this));
+    }
+  }
+}
+</script>
+
